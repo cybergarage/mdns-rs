28ae114e1320bed7c651061da13ba62bde551f5a
Satoshi Konno <skonno@cybergarage.org>
2024-03-15 00:15:58 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub use self::client::{Client};
+pub use self::client::Client;
 
 mod client;
 mod default;
