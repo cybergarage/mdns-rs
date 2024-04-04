b8e62f722bd8d3618a663b0e51a6a2dad18001f9
Satoshi Konno <skonno@cybergarage.org>
2024-04-04 12:48:50 +0900

    Update mod.rs

--- src/dns/mod.rs
+++ src/dns/mod.rs
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub use self::error::*;
 pub use self::message::*;
 
+mod error;
 mod message;
 mod message_test;
