5fd49ddc4d9c9c4c01222a27eb4a331d30726b7a
Satoshi Konno <skonno@cybergarage.org>
2024-04-11 08:38:20 +0900

    Add type constants

--- src/dns/typ.rs
+++ src/dns/typ.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub const TYPE_MASK: u16 = 0x7fff;
+
 #[derive(PartialEq)]
 pub enum Type {
     NONE = 0x0000,
