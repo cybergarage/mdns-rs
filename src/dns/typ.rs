1c2a1d8e243cf5e058b3e481e78f93c5355a906a
Satoshi Konno <skonno@cybergarage.org>
2024-04-11 08:03:58 +0900

    Update dns.Type

--- src/dns/typ.rs
+++ src/dns/typ.rs
@@ -14,15 +14,16 @@
 
 #[derive(PartialEq)]
 pub enum Type {
-    A,
-    NS,
-    CNAME,
-    SOA,
-    PTR,
-    MX,
-    TXT,
-    AAAA,
-    SRV,
-    OPT,
-    Unknown(u16),
+    A = 0x0001,
+    NS = 0x0002,
+    CNAME = 0x0005,
+    SOA = 0x0006,
+    PTR = 0x000c,
+    MX = 0x000f,
+    TXT = 0x0010,
+    AAAA = 0x001c,
+    SRV = 0x0021,
+    NAPTR = 0x0023,
+    OPT = 0x0029,
+    ANY = 0x00ff,
 }
