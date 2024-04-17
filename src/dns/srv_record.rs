949355195d77f2969945eea7128393903b3c1591
Satoshi Konno <skonno@cybergarage.org>
2024-04-18 00:39:59 +0900

    Add SRVRecord::from_record()

--- src/dns/srv_record.rs
+++ src/dns/srv_record.rs
@@ -12,13 +12,78 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use crate::dns::error::Error;
+use crate::dns::reader::Reader;
+use crate::dns::record::Record;
 use std::fmt;
 
-pub struct SRVRecord {}
+pub struct SRVRecord {
+    service: String,
+    proto: String,
+    name: String,
+    priority: u16,
+    weight: u16,
+    port: u16,
+    target: String,
+}
 
 impl SRVRecord {
-    pub fn new() -> SRVRecord {
-        SRVRecord {}
+    /// from_record creates a new SRV record from the specified record.
+    pub fn from_record(record: &Record) -> Result<SRVRecord, Error> {
+        let mut srv = SRVRecord {
+            service: "".to_string(),
+            proto: "".to_string(),
+            name: "".to_string(),
+            priority: 0,
+            weight: 0,
+            port: 0,
+            target: "".to_string(),
+        };
+        let data = record.data();
+        if data.len() == 0 {
+            return Ok(srv);
+        }
+        let mut reader = Reader::from_bytes(data);
+        srv.priority = reader.read_u16()?;
+        srv.weight = reader.read_u16()?;
+        srv.port = reader.read_u16()?;
+        srv.target = reader.read_name()?;
+        Ok(srv)
+    }
+
+    /// service returns the service of the SRV record.
+    pub fn service(&self) -> &str {
+        &self.service
+    }
+
+    /// proto returns the protocol of the SRV record.
+    pub fn proto(&self) -> &str {
+        &self.proto
+    }
+
+    /// name returns the name of the SRV record.
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// priority returns the priority of the SRV record.
+    pub fn priority(&self) -> u16 {
+        self.priority
+    }
+
+    /// weight returns the weight of the SRV record.
+    pub fn weight(&self) -> u16 {
+        self.weight
+    }
+
+    /// port returns the port of the SRV record.
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    /// target returns the target of the SRV record.
+    pub fn target(&self) -> &str {
+        &self.target
     }
 }
 
